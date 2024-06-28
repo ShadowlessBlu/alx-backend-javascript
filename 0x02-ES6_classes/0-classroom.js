e1db7bcfc7c3a94ddffe4efba9716c24dd59eaeb
calvin <calvinsewornu123@gmail.com>
2024-06-28 04:02:11 +0000

    classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represent a classroom.
+ */
+export default class ClassRoom {
+  /**
+   * Create  new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - Maximum number of
+   * students in class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
