146f2d8464f58307a60595e47bbee651e7b92831
calvin <calvinsewornu123@gmail.com>
2024-07-03 04:39:38 +0000

    data manipulate

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Sewornu Calvin <https://github.com/ShadowlessBlu>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
